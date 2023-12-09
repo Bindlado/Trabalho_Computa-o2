f7a3781ea1f41ff1616cd9dd0c52d7946cc8274d
Bindlado <diegogabriel011@gmail.com>
2023-12-09 18:35:06 -0300

    Agora com syspath

---
+++ Iniciar_o_projeto.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+# Obtém o diretório do __init__.py
+diretorio_do_arquivo = os.path.dirname(os.path.abspath(__file__))
+
+# Adiciona o diretório ao path para importar módulos de lá
+sys.path.append(diretorio_do_arquivo)
+
+# Muda o diretório de trabalho para o local do script
+os.chdir(os.path.join(diretorio_do_arquivo, 'View'))
+
+# Executa o script TelaLogin.py como um programa separado
+os.system('python TelaLogin.py')
